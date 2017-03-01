fd6b4b09a5059540f6339b30def1397efc6d07bb
stve <steve.agalloco@gmail.com>
2017-03-01 09:00:59 -0500

    fix IP regex in tests

--- test.js
+++ test.js
@@ -6,27 +6,27 @@ if (!process.env.CI) {
 		const alfy = alfyTest();
 		const result = await alfy('ip');
 
-		t.regex(result[0].arg, /(\d)+.(\d)+.(\d)+.(\d)+./);
+		t.regex(result[0].arg, /(\d)+\.(\d)+\.(\d)+\.(\d)+/);
+	});
+
+	test('local IP title', async t => {
+		const alfy = alfyTest();
+		const result = await alfy('ip');
+
+		t.regex(result[0].title, /Local IP: (\d)+\.(\d)+\.(\d)+\.(\d)+/);
 	});
 }
-
-test('local IP title', async t => {
-	const alfy = alfyTest();
-	const result = await alfy('ip');
-
-	t.regex(result[0].title, /Local IP: (\d)+.(\d)+.(\d)+.(\d)+./);
-});
 
 test('external IP arg', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('ip');
 
-	t.regex(result[1].arg, /(\d)+.(\d)+.(\d)+.(\d)+./);
+	t.regex(result[1].arg, /(\d)+\.(\d)+\.(\d)+\.(\d)+/);
 });
 
 test('external IP title', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('ip');
 
-	t.regex(result[1].title, /External IP: (\d)+.(\d)+.(\d)+.(\d)+./);
+	t.regex(result[1].title, /External IP: (\d)+\.(\d)+\.(\d)+\.(\d)+/);
 });
