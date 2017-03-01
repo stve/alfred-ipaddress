fc36ec18602dc9245d541010240e042d85990322
stve <steve.agalloco@gmail.com>
2017-03-01 08:57:35 -0500

    split out tests

--- test.js
+++ test.js
@@ -1,13 +1,32 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test(async t => {
+if (!process.env.CI) {
+	test('local IP arg', async t => {
+		const alfy = alfyTest();
+		const result = await alfy('ip');
+
+		t.regex(result[0].arg, /(\d)+.(\d)+.(\d)+.(\d)+./);
+	});
+}
+
+test('local IP title', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('ip');
 
-	t.regex(result[0].arg, /(\d)+.(\d)+.(\d)+.(\d)+./);
 	t.regex(result[0].title, /Local IP: (\d)+.(\d)+.(\d)+.(\d)+./);
+});
+
+test('external IP arg', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('ip');
 
 	t.regex(result[1].arg, /(\d)+.(\d)+.(\d)+.(\d)+./);
+});
+
+test('external IP title', async t => {
+	const alfy = alfyTest();
+	const result = await alfy('ip');
+
 	t.regex(result[1].title, /External IP: (\d)+.(\d)+.(\d)+.(\d)+./);
 });
